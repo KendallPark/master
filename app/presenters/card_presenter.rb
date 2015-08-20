187f381f0ff0157b2eecb2a96a4134514cf1287d
Kendall Park <darkidder@gmail.com>
2015-08-19 23:34:31 -0500

    corrected presenter

--- app/presenters/card_presenter.rb
+++ app/presenters/card_presenter.rb
@@ -12,7 +12,7 @@ class CardPresenter < BasePresenter
       tags: card.tag_list,
       front_image_thumb_url: card.content.front_image.present? ? card.content.front_image.url(:thumb) : nil,
       back_image_thumb_url: card.content.back_image.present? ? card.content.back_image.url(:thumb) : nil,
-      front_image_url: card.content.front_image.present? ? card.content.back_image.url(:medium) : nil,
+      front_image_url: card.content.front_image.present? ? card.content.front_image.url(:medium) : nil,
       back_image_url: card.content.back_image.present? ? card.content.back_image.url(:medium) : nil,
     }
   end
