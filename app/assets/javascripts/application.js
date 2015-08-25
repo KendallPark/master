af055b566a7641f876ef4a4a9fb1771dc2fa1bbb
Kendall Park <darkidder@gmail.com>
2015-08-24 20:31:03 -0500

    added polyfill for safari bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require react
 //= require react_ujs
 //= require react_bootstrap
+//= require babel/polyfill
 //= require classnames
 //= require react-input-autosize
 //= require react-select
