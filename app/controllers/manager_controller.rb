194b7c74b20efc48abdabaa2c0911163e13d26d8
Kendall Park <darkidder@gmail.com>
2015-09-07 21:18:03 -0500

    fixed browsing other users cards

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,6 +1,12 @@
 class ManagerController < ApplicationController
   def index
-    @browse_only = params[:username] != current_user.username
-    @cards = CardPresenter.present(current_user.cards.recent, {user: current_user})
+    if params[:username] != current_user.username
+      @browse_only = true
+      user = User.find_by({email_prefix: params[:username]})
+    else
+      @browse_only = false
+      user = current_user
+    end
+    @cards = CardPresenter.present(user.cards.recent, {user: current_user})
   end
 end
