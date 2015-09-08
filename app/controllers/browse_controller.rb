048bf2e22264afd72cbc9789032a85dca8cf0110
Kendall Park <darkidder@gmail.com>
2015-09-07 22:50:20 -0500

    Don't displayed edited remixed cards

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,5 +1,5 @@
 class BrowseController < ApplicationController
   def index
-    @cards = CardPresenter.present(Card.all.recent.where.not(user_id: current_user.id), {user: current_user})
+    @cards = CardPresenter.present(Card.all.recent.where.not(user_id: current_user.id).where('cards.original_content_id = cards.card_content_id'), {user: current_user})
   end
 end
