7e6b8a403f7ab591030b234daf65eab0c45f69e3
Kendall Park <darkidder@gmail.com>
2015-09-07 20:53:58 -0500

    updated card presenter

--- app/presenters/card_presenter.rb
+++ app/presenters/card_presenter.rb
@@ -1,6 +1,10 @@
 class CardPresenter < BasePresenter
-  def present(card)
+  def present(card, options={})
+    copied = {}
+    copied[:copied] = card.remixed_by?(options[:user]) if options[:user]
     { id: card.id,
+      owner_name: card.user.full_name,
+      owner_username: card.user.username,
       user_id: card.user.id,
       content_id: card.card_content_id,
       content_user_id: card.content.user.id,
@@ -14,6 +18,6 @@ class CardPresenter < BasePresenter
       back_image_thumb_url: card.content.back_image.present? ? card.content.back_image.url(:thumb) : nil,
       front_image_url: card.content.front_image.present? ? card.content.front_image.url(:medium) : nil,
       back_image_url: card.content.back_image.present? ? card.content.back_image.url(:medium) : nil,
-    }
+    }.merge(copied)
   end
 end
